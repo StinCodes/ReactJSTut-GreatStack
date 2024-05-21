4dde774ee299c6b880f31d8e67127c6a1f0d30b9
Austin Song <austinhssong@gmail.com>
2024-05-21 12:30:14 -0400

    first commit, setup complete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// const App = () => {
+//   let x = 'GreatStack'
+//   let array = ['user1', 'user2', 'user3', 'user4', 'user5']
+//   return (
+//     <div>
+//       {x}
+//       {array.map((user)=>{
+//         return <h2>{user}</h2>
+//       })}
+//     </div>
+//   );
+// };
+
+const App = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default App;
